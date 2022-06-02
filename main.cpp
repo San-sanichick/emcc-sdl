e8dd3d5f2ae9e348307b2a74f88fe35a26b5e112
Alexander <sansanichick@gmail.com>
2022-06-02 17:13:40 +1000

    fix: main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "bench/Benchmark.h"
 
+// #define BUILD_MAIN 1
+
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
-// void loop(void* arg) {
-//     static_cast<Benchmark*>(arg)->loop();
-// }
+void loop(void* arg) {
+    static_cast<Benchmark*>(arg)->loop();
+}
 
 int main() {
     printf("hello world\n");
@@ -24,7 +26,7 @@ int main() {
 
 #ifdef __EMSCRIPTEN__
         // we can't exactly use an infinite while loop in a browser, so we use this
-        // emscripten_set_main_loop_arg(&loop, &bench, 0, 1);
+        emscripten_set_main_loop_arg(&loop, &bench, 0, 1);
 #else
         while(true) {
             bench.update();
